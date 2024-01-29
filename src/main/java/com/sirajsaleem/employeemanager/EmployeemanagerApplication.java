6b00f0dd9703d1490e65f1e6e2de12a8ffff6bba
Siraj Mohammed Saleem <sirajms5@yahoo.com>
2024-01-28 19:36:03 -0500

    added db

--- src/main/java/com/sirajsaleem/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/sirajsaleem/employeemanager/EmployeemanagerApplication.java
@@ -2,8 +2,10 @@ package com.sirajsaleem.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"model"})
 public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,94 @@
+package model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
+	private long id;
+	private String name;
+	private String email;
+	private String jobTitle;
+	private String phoneNumber;
+	private String imageUrl;
+	@Column(nullable = false, updatable = false)
+	private String employeeCode;
+	
+	public Employee() {}
+	
+	public Employee(long id, String name, String email, String jobTitle, String phoneNumber, String imageUrl,
+			String employeeCode) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.jobTitle = jobTitle;
+		this.phoneNumber = phoneNumber;
+		this.imageUrl = imageUrl;
+		this.employeeCode = employeeCode;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getEmployeeCode() {
+		return employeeCode;
+	}
+
+	public void setEmployeeCode(String employeeCode) {
+		this.employeeCode = employeeCode;
+	}
+	
+}
